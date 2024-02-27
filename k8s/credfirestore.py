7880b164ae9b5f6bfb53f419e4ae9f9d057bc8bf
ivan <iv.morenoma@gmail.com>
2024-02-27 18:00:13 +0100

    update example data with real json

--- k8s/credfirestore.py
+++ k8s/credfirestore.py
@@ -5,7 +5,7 @@ import json
 load_dotenv()
 
 def firebase_cred():    
-
+    FIREBASE_TYPE = os.getenv('FIREBASE_TYPE')
     FIRESTORE_PROJECT_ID = os.getenv('FIRESTORE_PROJECT_ID')
     FIRESTORE_PRIVATE_KEY_ID = os.getenv('FIRESTORE_PRIVATE_KEY_ID')
     FIRESTORE_PRIVATE_KEY = os.getenv('FIRESTORE_PRIVATE_KEY').replace('\\n','\n')
@@ -15,17 +15,20 @@ def firebase_cred():
     FIRESTORE_TOKEN_URI = os.getenv('FIRESTORE_TOKEN_URI')
     FIRESTORE_AUTH_PROVIDER_X509_CERT_URL = os.getenv('FIRESTORE_AUTH_PROVIDER_X509_CERT_URL')
     FIRESTORE_CLIENT_X509_CERT_URL = os.getenv('FIRESTORE_CLIENT_X509_CERT_URL')
+    FIREBASE_UNIVERSE_DOMAIN = os.getenv('FIREBASE_UNIVERSE_DOMAIN')
 
     cred = {
-        'firestore_project_id': FIRESTORE_PROJECT_ID,
-        'firestore_private_key_id': FIRESTORE_PRIVATE_KEY_ID,
-        'firestore_private_key': FIRESTORE_PRIVATE_KEY,
-        'firestore_client_email': FIRESTORE_CLIENT_EMAIL,
-        'firestore_client_id': FIRESTORE_CLIENT_ID,
-        'firestore_auth_uri': FIRESTORE_AUTH_URI,
-        'firestore_token_uri': FIRESTORE_TOKEN_URI,
-        'firestore_auth_provider_X509_cert_url':FIRESTORE_AUTH_PROVIDER_X509_CERT_URL,
-        'firestore_client_X509_cert_url': FIRESTORE_CLIENT_X509_CERT_URL
+        "type": FIREBASE_TYPE,
+        'project_id': FIRESTORE_PROJECT_ID,
+        'private_key_id': FIRESTORE_PRIVATE_KEY_ID,
+        'private_key': FIRESTORE_PRIVATE_KEY,
+        'client_email': FIRESTORE_CLIENT_EMAIL,
+        'client_id': FIRESTORE_CLIENT_ID,
+        'auth_uri': FIRESTORE_AUTH_URI,
+        'token_uri': FIRESTORE_TOKEN_URI,
+        'auth_provider_X509_cert_url':FIRESTORE_AUTH_PROVIDER_X509_CERT_URL,
+        'client_X509_cert_url': FIRESTORE_CLIENT_X509_CERT_URL,
+        'universe_domain': FIREBASE_UNIVERSE_DOMAIN
     }
 
     cred = json.dumps(cred, indent=4)
