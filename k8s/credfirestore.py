2848755877955c1777927fe27e53f460ec1da719
ivan <iv.morenoma@gmail.com>
2024-02-28 20:08:33 +0100

    refactor variable names

--- k8s/credfirestore.py
+++ k8s/credfirestore.py
@@ -4,21 +4,28 @@ import json
 
 load_dotenv()
 
-def firebase_cred():    
-    FIREBASE_TYPE = os.getenv('FIREBASE_TYPE')
-    FIRESTORE_PROJECT_ID = os.getenv('FIRESTORE_PROJECT_ID')
-    FIRESTORE_PRIVATE_KEY_ID = os.getenv('FIRESTORE_PRIVATE_KEY_ID')
-    FIRESTORE_PRIVATE_KEY = os.getenv('FIRESTORE_PRIVATE_KEY').replace('\\n','\n')
-    FIRESTORE_CLIENT_EMAIL = os.getenv('FIRESTORE_CLIENT_EMAIL')
-    FIRESTORE_CLIENT_ID = os.getenv('FIRESTORE_CLIENT_ID')
-    FIRESTORE_AUTH_URI = os.getenv('FIRESTORE_AUTH_URI')
-    FIRESTORE_TOKEN_URI = os.getenv('FIRESTORE_TOKEN_URI')
-    FIRESTORE_AUTH_PROVIDER_X509_CERT_URL = os.getenv('FIRESTORE_AUTH_PROVIDER_X509_CERT_URL')
-    FIRESTORE_CLIENT_X509_CERT_URL = os.getenv('FIRESTORE_CLIENT_X509_CERT_URL')
-    FIREBASE_UNIVERSE_DOMAIN = os.getenv('FIREBASE_UNIVERSE_DOMAIN')
+def firebase_cred():
+
+    FIRESTORE_TYPE = os.getenv('FIREBASE_TYPE')
+    FIRESTORE_PROJECT_ID = os.getenv('FIREBASE_PROJECT_ID')
+    FIRESTORE_PRIVATE_KEY_ID = os.getenv('FIREBASE_PRIVATE_KEY_ID')
+
+    FIRESTORE_PRIVATE_KEY = os.getenv('FIREBASE_PRIVATE_KEY')
+    if FIRESTORE_PRIVATE_KEY is None:
+        raise Exception('FIREBASE_PRIVATE_KEY environment variable is not set')
+        
+    FIRESTORE_PRIVATE_KEY = FIRESTORE_PRIVATE_KEY.replace('\\n', '\n')
+    
+    FIRESTORE_CLIENT_EMAIL = os.getenv('FIREBASE_CLIENT_EMAIL')
+    FIRESTORE_CLIENT_ID = os.getenv('FIREBASE_CLIENT_ID')
+    FIRESTORE_AUTH_URI = os.getenv('FIREBASE_AUTH_URI')
+    FIRESTORE_TOKEN_URI = os.getenv('FIREBASE_TOKEN_URI')
+    FIRESTORE_AUTH_PROVIDER_X509_CERT_URL = os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL')
+    FIRESTORE_CLIENT_X509_CERT_URL = os.getenv('FIREBASE_CLIENT_X509_CERT_URL')
+    FIRESTORE_UNIVERSE_DOMAIN = os.getenv('FIREBASE_UNIVERSE_DOMAIN')
 
     cred = {
-        "type": FIREBASE_TYPE,
+        'type': FIRESTORE_TYPE,
         'project_id': FIRESTORE_PROJECT_ID,
         'private_key_id': FIRESTORE_PRIVATE_KEY_ID,
         'private_key': FIRESTORE_PRIVATE_KEY,
@@ -28,9 +35,8 @@ def firebase_cred():
         'token_uri': FIRESTORE_TOKEN_URI,
         'auth_provider_X509_cert_url':FIRESTORE_AUTH_PROVIDER_X509_CERT_URL,
         'client_X509_cert_url': FIRESTORE_CLIENT_X509_CERT_URL,
-        'universe_domain': FIREBASE_UNIVERSE_DOMAIN
+        'universe_domain': FIRESTORE_UNIVERSE_DOMAIN
     }
 
-    cred = json.dumps(cred, indent=4)
-
+    cred = json.dumps(cred,indent=4).replace('\\n', '\n')
     return cred
